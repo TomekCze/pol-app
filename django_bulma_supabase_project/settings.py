af235cff83756dc3587024b6bc23429a167fca80
Tomek <tomek.czernecki@gmail.com>
2025-05-02 13:28:39 +0200

    Static files2

--- django_bulma_supabase_project/settings.py
+++ django_bulma_supabase_project/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Application definition
 
